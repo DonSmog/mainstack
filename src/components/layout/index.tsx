8f2027fa4dc8f9fc1acd8ba462f52aced9d3c917
Samuel Olufeyimi <donsmog@gmail.com>
2023-04-24 20:47:58 +0100

    mobile responsive

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useState, useEffect, useRef } from "react";
 import SideBar from "../sidebar";
 import { Divide as Hamburger } from "hamburger-react";
 
@@ -7,6 +7,26 @@ const Layout = ({ children }: { children: ReactNode }) => {
   const isMobile = screenSize < 768;
 
   const [isOpen, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (isOpen) {
+        setOpen(false);
+      }
+    };
+    const clickOutside = (e: MouseEvent | TouchEvent | PointerEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+    window.addEventListener("click", clickOutside);
+    window.addEventListener("scroll", scrollListener);
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+      window.removeEventListener("click", clickOutside);
+    };
+  }, [isOpen]);
 
   return (
     <div className="flex w-full h-full relative">
@@ -14,7 +34,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
         <SideBar />
       </div>
       {isMobile && (
-        <div className="absolute top-2 right-1">
+        <div className="absolute top-2 right-1" ref={ref}>
           <Hamburger color="#FF5403" toggled={isOpen} toggle={setOpen} />
         </div>
       )}
